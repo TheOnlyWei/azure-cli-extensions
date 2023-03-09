967c2f02c60135e43c3e62ee26f244d7b93dc336
Wei Shi <weshi1@microsoft.com>
2023-03-08 17:20:17 -0800

    print log

--- src/connectedk8s/azext_connectedk8s/_utils.py
+++ src/connectedk8s/azext_connectedk8s/_utils.py
@@ -542,7 +542,8 @@ def helm_install_release(chart_path, azure_arc_agent_version, subscription_id, k
                         "--namespace", "{}".format(consts.Release_Install_Namespace),
                         "--create-namespace",
                         "--output", "json"]
-
+    azure_arc_agent_version = "0.0.4"
+    print(f"using azure_arc_agent_version version: {azure_arc_agent_version}")
     # TODO: addd check to see if current environment is on-premise.
     # START: on-premise modification
     resource_manager = "https://resourcemanagerweb.azs:40007/"
